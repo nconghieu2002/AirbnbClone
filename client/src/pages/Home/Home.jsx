2e15eb062ff36739f29fc195f4586d176668e5a4
nconghieu2002 <nconghieu2002@gmail.com>
2023-05-23 21:03:25 +0700

    update

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -6,34 +6,41 @@ import Image from '@/components/Image';
 
 function Home() {
     const [places, setPlaces] = useState([]);
-
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         axios.get('places').then((response) => {
             setPlaces(response.data);
+            setLoading(false);
         });
     }, []);
 
     return (
-        <div className="gap-x-6 gap-y-8 mt-8 grid grid-cols-3 sm:grid-cols-2  lg:grid-cols-4">
-            {places.length > 0 &&
-                places.map((place) => (
-                    <Link to={`/place/${place._id}`} key={place._id}>
-                        <div className="bg-gray-500 rounded-2xl flex mb-2 hover:scale-105 duration-500">
-                            {place.photos?.length > 0 && (
-                                <Image
-                                    className="object-cover aspect-square rounded-2xl "
-                                    src={place.photos[0]}
-                                    alt=""
-                                />
-                            )}
-                        </div>
-                        <h3 className="text-sm font-bold truncate">{place.address}</h3>
-                        <h2 className="text-sm text-gray-500 truncate">{place.title}</h2>
-                        <div className="mt-1">
-                            <span className="font-bold text-primary">${place.price}</span> per night
-                        </div>
-                    </Link>
-                ))}
+        <div>
+            {loading ? (
+                <div>loading...</div>
+            ) : (
+                <div className="gap-x-6 gap-y-8 mt-8 grid grid-cols-3 sm:grid-cols-2  lg:grid-cols-4">
+                    {places.length > 0 &&
+                        places.map((place) => (
+                            <Link to={`/place/${place._id}`} key={place._id}>
+                                <div className="bg-gray-500 rounded-2xl flex mb-2 hover:scale-105 duration-500">
+                                    {place.photos?.length > 0 && (
+                                        <Image
+                                            className="object-cover aspect-square rounded-2xl "
+                                            src={place.photos[0]}
+                                            alt=""
+                                        />
+                                    )}
+                                </div>
+                                <h3 className="text-sm font-bold truncate">{place.address}</h3>
+                                <h2 className="text-sm text-gray-500 truncate">{place.title}</h2>
+                                <div className="mt-1">
+                                    <span className="font-bold text-primary">${place.price}</span> per night
+                                </div>
+                            </Link>
+                        ))}
+                </div>
+            )}
         </div>
     );
 }
